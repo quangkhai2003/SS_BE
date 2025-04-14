87f86f5db186a5a6bf6ec666b5116bc04d51e66b
pt-hung03-tech <pt.hung03.tech@gmail.com>
2025-04-14 10:13:38 +0700

    Dictionary

--- app/Services/DictionaryService.php
+++ app/Services/DictionaryService.php
@@ -1,8 +1,28 @@
 <?php
 
 namespace App\Services;
+use App\Models\Dictionary;
+use Dedoc\Scramble\Attributes\Example;
+
+use function PHPUnit\Framework\exactly;
 
 class DictionaryService
-{
+{        
+    public function getTopWordsByTopic() {
+        $topics = Dictionary::select('topic')
+            ->distinct()
+            ->pluck('topic');
     
+        $result = [];
+        foreach ($topics as $topic) {
+            $words = Dictionary::where('topic', $topic)
+                ->orderBy('dictionary_id', 'asc')
+                ->limit(5)
+                ->get(['dictionary_id', 'word', 'ipa', 'word_type', 'vietnamese', 'examples', 'examples_vietnamese', 'topic']);
+    
+                $result[$topic] = $words;
+            }
+    
+        return $result;
+    }
 }
